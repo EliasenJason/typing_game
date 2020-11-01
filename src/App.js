aff4e80f9c54fdd70ee21e6b4b33974ddf539e9b
Jason Eliasen <eliasenjason@gmail.com>
2020-11-01 10:48:41 -0700

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const STARTING_TIME = 2
+    
+    const [text, setText] = React.useState("")
+    const [timeRemaining, setTimeRemaining] = React.useState(STARTING_TIME)
+    const [isGameRunning, setisGameRunning] = React.useState(false)
+    const [wordCount, setWordCount] = React.useState(0)
+    const [displayEndGame, setDisplayEndGame] = React.useState(false)
+    const textBoxRef = React.useRef(null)
+    
+    
+    const textAreaOnChangeHandler = (event) => {
+        setText(event.target.value)
+    }
+
+    const startGame = () => {
+        setisGameRunning(true)
+        setTimeRemaining(STARTING_TIME)
+        setText("")
+        setDisplayEndGame(false)
+    }
+
+    React.useEffect(() => {
+        if(displayEndGame && !isGameRunning) {
+            let arrayOfWords = text.split(' ').filter(item => item.length > 0)
+            setWordCount(arrayOfWords.length)
+            console.log(arrayOfWords)
+        }
+    },[displayEndGame])
+    
+    React.useEffect(() => {
+        if(isGameRunning && timeRemaining > 0) { //start the game
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+        } else if(timeRemaining === 0) { //end the game
+            setisGameRunning(false)
+            setDisplayEndGame(true)
+        }
+    }, [timeRemaining, isGameRunning])
+    
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea
+                ref={textBoxRef}
+                value={text}
+                onChange={textAreaOnChangeHandler}
+                disabled={!isGameRunning}
+            />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button 
+                onClick={startGame}
+                disabled={isGameRunning}
+            >
+                Start
+            </button>
+            <h1>Word count: {wordCount}</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
